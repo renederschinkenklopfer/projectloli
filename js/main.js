53cc7b1e1ecc7d9430060f43f577ecf595b2640a
renederschinkenklopfer <renederschinkenklopfer@gmail.com>
2015-05-04 21:55:31 +0200

    mobile create thread button fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
-	$("#newThread").click(function() {
+	$(".newThread").click(function() {
 	  $('.navbar-collapse').collapse('hide');
 	  $("#postThread").show( "fast", function() {
 	  });
 	});
 
-	document.getElementById("uploadBtn").onchange = function () {  
+	document.getElementById("uploadBtn").onchange = function () {
 		document.getElementById("uploadFile").value = this.value.replace(/^.*\\/, "");;
 	};
 });
